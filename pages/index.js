c257bf7e3a26f7e7268bfd6194e04749b51875d3
w27730cs <cosmin.serdean@student.manchester.ac.uk>
2020-04-13 01:03:38 +0300

    Some styling

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import Head from 'next/head';
 
 import { Client } from '../prismic';
 
 const Index = ({ doc: { data } }) => {
   const [{ text: title }] = data.title;
+  const [{ text: icon }] = data.icon;
   const [{ text: welcomeText }] = data['welcome-text'];
 
-  console.log(title, welcomeText);
   return (
     <div>
+      <Head>
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${icon}</text></svg>`}
+        />
+      </Head>
+      <h1 className="icon">{icon}</h1>
       <h1>{title}</h1>
       <p>{welcomeText}</p>
+      <style jsx>
+        {`
+          @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+          h1,
+          p {
+            font-family: 'Playfair Display', serif;
+            color: #484848;
+          }
+
+          .icon {
+            font-size: 4rem;
+          }
+
+          div {
+            max-width: 700px;
+            margin: 20rem auto;
+          }
+        `}
+      </style>
     </div>
   );
 };
 
 export const getStaticProps = async () => {
   const doc = await Client().getSingle('homepage');
-
-  console.log(doc);
 
   return { props: { doc } };
 };
